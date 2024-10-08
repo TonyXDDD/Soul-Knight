07c61cef923a545c656893c60d71c70200cb29a2
TonyXDDD <joshua20.alfarano@gmail.com>
2024-10-08 12:27:44 -0400

    Dash added do Knight

--- scenes/audio_stream_player_2d.gd
+++ scenes/audio_stream_player_2d.gd
@@ -1,8 +1,17 @@
 extends AudioStreamPlayer2D
 
-# This function is called when the node enters the scene tree for the first time.
-func _ready():
-	# Ensure the audio is set to loop
-	stream.set_loop(true)
-	# Play the audio
-	play()
+@onready var audio_stream_player_2d: AudioStreamPlayer2D = $"."
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_finished() -> void:
+	$".".play()
