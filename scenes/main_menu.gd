4b8f9cdeb142bb521f0057c3f2d124b4da307e69
TonyXDDD <joshua20.alfarano@gmail.com>
2024-10-17 16:10:33 -0400

    to upload to itch.io

--- scenes/main_menu.gd
+++ scenes/main_menu.gd
@@ -6,11 +6,14 @@ extends Control
 @onready var tutrl: Button = $MarginContainer/HBoxContainer/VBoxContainer/tutrl as Button
 @onready var start_level = preload("res://scenes/game.tscn") as PackedScene
 @onready var start_levelT = preload("res://scenes/tutorial_level.tscn") as PackedScene
+@onready var transition: Control = $"Transition"
+@onready var animation_player: AnimationPlayer = $"Transition/AnimationPlayer"
 
 func _ready():
 	start_b.button_down.connect(on_start_pressed)
 	quit_b.button_down.connect(on_exit_pressed)
 	tutrl.button_down.connect(on_tutrl_pressed)
+	animation_player.play("fade_in")
 
 func on_start_pressed() -> void:
 	get_tree().change_scene_to_packed(start_level)
